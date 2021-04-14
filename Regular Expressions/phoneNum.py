0f6b74f04e128b2acac6b0e794452e317556b78d
JonSnowlo <scott.c.court@protonmail.com>
2021-04-14 12:51:52 -0400

    Split regExpressions.py into multiple files for ease of review

--- Regular Expressions/phoneNum.py
+++ Regular Expressions/phoneNum.py
@@ -4,29 +4,9 @@ phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') # pass desired pattern to 
 mo = phoneNumRegex.search('My number is 415-555-4242.') # call search() on phoneNumRegex and pass the string we want to search for the regex object in phoneNumRegex.
 print('Phone number found: ' + mo.group()) # print the Match Object
 
-phoneNumRegex = re.compile(r'(\d{3})-(\d{3}-\d{4})')
-mo = phoneNumRegex.search('My number is 415-555-4242.')
-print(mo.group(1))
-print(mo.group(2))
-print(mo.group(0))
-print(mo.group())
-
-print(mo.groups())
-areaCode, mainNumber = mo.groups()
-print(areaCode)
-print(mainNumber)
-
 phoneNumRegex = re.compile(r'(\(\d{3}\)) (\d{3}-\d{4})')
 mo = phoneNumRegex.search('My number is (415) 555-4242.')
 print(mo.group(1))
 print(mo.group(2))
 
-heroRegex = re.compile(r'Batman|Tina Fey')
-mo1 = heroRegex.search('Batman and Tina Fey')
-print(mo1.group())
-
-mo2 = heroRegex.search('Tina Fey and Batman')
-print(mo2.group())
-
 phoneNumRegex = re.compile(r'((\()?\d{3}(\))?)( |-)(\d{3}( |-)\d{4})') # find phone number in any commonly used format
-
