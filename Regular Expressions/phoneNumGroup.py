566d8d47d8addf391160c397e2395185ce55d2a1
JonSnowlo <scott.c.court@protonmail.com>
2021-04-14 14:22:13 -0400

    changed regex compiler to verbose

--- Regular Expressions/phoneNumGroup.py
+++ Regular Expressions/phoneNumGroup.py
@@ -1,6 +1,14 @@
 import re
 
-phoneNumRegex = re.compile(r'(\d{3})-(\d{3}-\d{4})')
+phoneNumRegex = re.compile(r'''(  # find phone number in any commonly used format with or without an area code.
+    ((\()?\d{3}(\))?)  # area code with or without parentheses.
+    (\s|-|\.)?  # separator.
+    \d{3}  # first 3 digits.
+    (\s|-|\.)?  # separator.
+    \d{4}  # last 4 digits.
+    (\s*(ext|x|ext.)\s*\d{2,5})?  # extension.
+    )''', re.VERBOSE)
+    
 mo = phoneNumRegex.search('My number is 415-555-4242.')
 print(mo.group(1))
 print(mo.group(2))
