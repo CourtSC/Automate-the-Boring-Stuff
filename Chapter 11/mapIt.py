a1ee6c1f3d594fbd29b438754fcc95216d30f1ba
Scott Court <scott.c.court@gmail.com>
2021-11-24 09:19:59 -0500

    Added functionality to search address from clipboard in mapIt.py

--- Chapter 11/mapIt.py
+++ Chapter 11/mapIt.py
@@ -6,5 +6,8 @@ import webbrowser, sys, pyperclip
 if len(sys.argv) > 1:
     # Get address from command line.
     address = ' '.join(sys.argv[1:])
+else:
+    # Get address for clipboard.
+    address = pyperclip.paste()
 
-# TODO: Get address from clipboard.
+webbrowser.open('https://www.google.com/maps/place/' + address)
